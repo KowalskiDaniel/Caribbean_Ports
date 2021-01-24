2bbf68eb67b86d7d18db4737dcc1d5f2f8e4066e
KowalskiDaniel <dextrim.0x@gmail.com>
2021-01-24 20:01:15 +0100

    Manifest and serviceWorker

---
+++ serviceWorker.js
@@ -0,0 +1,33 @@
+var CACHE_NAME = 'caribbean-ports';
+var urlsToCache = [
+    '/',
+    './style.css',
+    './index.html',
+    './ports.html',
+    './background.png',
+    './myScript.js'
+];
+
+self.addEventListener('install', function(event) {
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(function(cache) {
+            console.log('Opened cache');
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+        .then(function(response) {
+            // Cache hit - return response
+            if (response) {
+                return response;
+            }
+            return fetch(event.request);
+        })
+    );
+});
